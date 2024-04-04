0d7458270c756d70dfc6cefbf935468695639716
Autumn <130979854+attd04@users.noreply.github.com>
2024-04-04 21:06:16 +0300

    Create paddle.py

---
+++ 100 days of code/turtle library/pong/paddle.py
@@ -0,0 +1,20 @@
+
+# paddle.py
+from turtle import Turtle
+
+class Paddle(Turtle):
+    def __init__(self, x_cord, y_cord):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(5,1)
+        self.penup()
+        self.goto(x_cord, y_cord)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
