a3f629f949c612bda5c6f2aa3b03c04717c3d2f6
Autumn <130979854+attd04@users.noreply.github.com>
2024-04-04 21:33:29 +0300

    Delete beginner projects/band name generator - day 1/main.py

--- beginner projects/band name generator - day 1/main.py
+++
@@ -1,7 +0,0 @@
-// DAY 1- Band Name Generator
-
-print("This is the Band Name Generator")
-city_name = input("What city did you grow up in?\n")  
-pet_name = input("What's your pets name?\n")
-print("YOUR BAND NAME: " + city_name + " " + pet_name)
-ax
