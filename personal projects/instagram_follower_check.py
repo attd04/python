7ff6a05de909b2cd9806a8e3cd903a7d53017ce3
Autumn <130979854+attd04@users.noreply.github.com>
2024-05-03 18:50:59 +0300

    Update instagram_follower_check.py

--- personal projects/instagram_follower_check.py
+++ personal projects/instagram_follower_check.py
@@ -1,13 +1,16 @@
 import instaloader
 
+username = input("Username:")
+password = input("Password:")
 # make instance
 loader = instaloader.Instaloader()
 
-# Login 
-loader.login("autumntiede", "PASSWORD")
+# Login using the credentials
+loader.login(username, password)
 
-# Use profile class to access metadata of account
-profile = instaloader.Profile.from_username(loader.context, 'autumntiede')
+# Use Profile class to access metadata of account
+profile = instaloader.Profile.from_username(loader.context,
+                                            username)
 
 # get followers
 followers = profile.get_followers()
@@ -21,7 +24,9 @@ followees_list = []
 for followee in followees:
     followees_list.append(followee)
 
+
 # compare lists and print who's not following back
 not_following = set(followees_list) - set(followers_list)
 for user in not_following:
     print(f"{user}\n")
+    
